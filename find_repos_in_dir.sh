066bc86e796b25bd3b36f4ed9d07e2536fdb3dd2
Matthew L Davis <mld.0@protonmail.com>
2022-05-25 02:45:10 +1000

    implement search for top-level/all repos

--- find_repos_in_dir.sh
+++ find_repos_in_dir.sh
@@ -19,92 +19,15 @@ trap '[ "$?" -ne 2 ] || exit 2' ERR
 
 
 path_testdir="$HOME"
+flag_debug=0
 
-
-_validate_find_repos_dir() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-	local func_about="about"
-	local func_help="""$func_name, $func_about
-	\$1					path_dir
-	-h | --help"""
-	#	{{{
-	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
-		echo "$func_help"
-		return 2
-	fi
-	#	}}}
-	local path_dir="${1:-}"
-	if [[ -d "$path_dir" ]]; then
-		echo "$func_name, error, dir not found, path_dir=($path_dir)" > /dev/stderr
-		exit 2
+log_debug() {
+#	{{{
+	if [[ $flag_debug -ne 0 ]]; then
+		echo "$@" > /dev/stderr
 	fi
 }
-
-_validate_is_repo_toplevel() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-	local func_about="about"
-	local func_help="""$func_name, $func_about
-	\$1					path_repo
-	-h | --help"""
-	#	{{{
-	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
-		echo "$func_help"
-		return 2
-	fi
-	#	}}}
-	local path_repo="${1:-}"
-	#	Continue: 2022-05-18T22:22:51AEST is 'path_repo' the top level of a git repo <(treatment of nested repos?)>
-	echo "UNIMPLEMENTED" > /dev/stderr
-	exit 2
-}
-
-_filter_repo() {
-	#	{{{
-	local func_name=""
-	if [[ -n "${ZSH_VERSION:-}" ]]; then 
-		func_name=${funcstack[1]:-}
-	elif [[ -n "${BASH_VERSION:-}" ]]; then
-		func_name="${FUNCNAME[0]:-}"
-	else
-		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
-	fi
-	#	}}}
-	local func_about="Determine whether a given repo should be excluded from output"
-	local func_help="""$func_name, $func_about
-	\$1					path_repo
-	-h | --help"""
-	#	{{{
-	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
-		echo "$func_help"
-		return 2
-	fi
-	#	}}}
-	local path_repo="${1:-}"
-	#	Continue: 2022-05-18T22:22:03AEST logic to decide which repos to exclude
-	if [[ ! -z "$path_repo" ]]; then
-		echo "true"
-	else
-		echo "false"
-	fi
-}
+#	}}}
 
 find_repos_in_dir() {
 	#	{{{
@@ -117,9 +40,11 @@ find_repos_in_dir() {
 		printf "%s\n" "warning, func_name unset, non zsh/bash shell" > /dev/stderr
 	fi
 	#	}}}
+	local flag_top_only=0
 	local func_about="Find all git repos in directory (recursive)"
 	local func_help="""$func_name, $func_about
 	\$1					path_dir (default \$PWD)
+	-T | --top			do not descend into repos
 	-h | --help"""
 	#	{{{
 	if echo "${1:-}" | perl -wne '/^\s*-h|--help\s*$/ or exit 1'; then
@@ -128,13 +53,84 @@ find_repos_in_dir() {
 	fi
 	#	}}}
 	local path_dir="${1:-$PWD}"
+	if [[ ! -d "$path_dir" ]]; then
+		echo "$func_name, error, dir not found, path_dir=($path_dir)" > /dev/stderr
+		exit 2
+	fi
+	#	{{{
+	for arg in "$@"; do
+		case $arg in
+			-h|--help)
+				echo "$func_help"
+				return 2
+				shift
+				;;
+			-v|--debug)
+				flag_debug=1
+				shift
+				;;
+			-T|--top)
+				flag_top_only=1
+				shift
+				;;
+		esac
+	done
+	#	}}}
+
+
+	path_dir=$( realpath "$path_dir" )
+	log_debug "$func_name, path_dir=($path_dir)"
 	_validate_find_repos_dir "$path_dir"
 
 	#	Continue: 2022-05-18T22:24:51AEST find repos in 'path_dir' recursively
 	#	LINK: https://stackoverflow.com/questions/11981716/how-to-quickly-find-all-git-repos-under-a-directory
+	#	LINK: https://unix.stackexchange.com/questions/333862/how-to-find-all-git-repositories-within-given-folders-fast
+
+	if [[ $flag_top_only -ne 0 ]]; then
+		#	{{{
+		#	Solution if pruning below .git is enough
+		#find "$path_dir" -type d -path '*/.git' -print -prune | xargs -I {} dirname {} 2> /dev/null
+		#find "$path_dir" -type d -path '*/.git' -printf '%h\n' -prune 2> /dev/null
+		#	Solution if the whole folder tree should be pruned once a .git is found
+		#for d in $path_dir; do
+		#	  find "$d" -type d -name .git -print -quit
+		#done | xargs -I {} dirname {}
+		#	}}}
+		find "$path_dir/" \( -exec test -d '{}'/.svn \; -or \
+		   -exec test -d {}/.git \; -or -exec test -d {}/CVS \; \) \
+		   -print -prune
+	else
+		#	Solution not pruning below .git
+		find "$path_dir" -type d -name .git 2> /dev/null | xargs -I {} dirname {}
+	fi
 
 }
 
+_validate_find_repos_dir() {
+	local path_dir="${1:-}"
+	if [[ ! -d "$path_dir" ]]; then
+		echo "$func_name, error, dir not found, path_dir=($path_dir)" > /dev/stderr
+		exit 2
+	fi
+}
+
+
+_filter_repo() {
+	local path_repo="${1:-}"
+	#	Continue: 2022-05-18T22:22:03AEST logic to decide which repos to exclude
+	if [[ ! -z "$path_repo" ]]; then
+		echo "true"
+	else
+		echo "false"
+	fi
+}
+
+#_validate_is_repo_toplevel() {
+#	local path_repo="${1:-}"
+#	#	Continue: 2022-05-18T22:22:51AEST is 'path_repo' the top level of a git repo <(treatment of nested repos?)>
+#	echo "UNIMPLEMENTED" > /dev/stderr
+#	exit 2
+#}
 
 check_sourced=1
 #	{{{
@@ -150,6 +146,6 @@ else
 fi
 #	}}}
 if [[ "$check_sourced" -eq 0 ]]; then
-	echo "" > /dev/null
+	find_repos_in_dir "$@"
 fi
 
